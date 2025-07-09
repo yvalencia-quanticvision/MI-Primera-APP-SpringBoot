441a5053d4fc0fdbf46b30257d81ca1cfe5ce9b6
yvalencia-quanticvision <alejandro.yandd@gmail.com>
2025-07-09 14:32:46 -0500

    Se hace la conexion a la BD MYSQL y se crea la clase Producto

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,11 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.3")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	runtimeOnly ("com.mysql:mysql-connector-j:9.3.0")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -0,0 +1,29 @@
+package com.platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVentas;
+
+    private boolean estado;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+}
